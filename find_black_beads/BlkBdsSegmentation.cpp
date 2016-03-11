2f69c0547b2252da084f925895429296dd450222
lusu8892 <sxl924@case.edu>
2016-03-11 17:32:18 -0500

    updated

--- find_black_beads/BlkBdsSegmentation.cpp
+++ find_black_beads/BlkBdsSegmentation.cpp
@@ -26,31 +26,49 @@ int main(int argc, char** argv )
     for(int i=0 ;; i++)
     {
         Mat frame;
+        Mat cvt_frame;
+        Mat threshold_frame;
+        Mat morph_frame;
 
         vector<vector<Point> > contours;
+        vector<vector<Point> > modifed_contours;
+
         vector<Vec4i> hierarchy;
+        vector<double> area_vec;
 
         cap >> frame; // get a new frame from camera
-        cvtColor(frame, frame, CV_BGR2GRAY); // convert color image to gray image
+        cvtColor(frame, cvt_frame, CV_BGR2GRAY); // convert color image to gray image
         // imshow("Black Beads", frame);
         //if(waitKey(30) >= 0) break;
-        threshold(frame, frame, 150, 255, THRESH_BINARY_INV+THRESH_OTSU); // segmentation
+        threshold(cvt_frame, threshold_frame, 150, 255, THRESH_BINARY_INV+THRESH_OTSU); // segmentation
         
-        morphologyEx(frame, frame, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE,Size(3,3)));
-        imshow("Black Beads", frame);
+        morphologyEx(threshold_frame, morph_frame, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE,Size(3,3)));
+        imshow("Black Beads", morph_frame);
         if(waitKey(30) >= 0) break;
-        findContours(frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-        cout<<contours.size();
-        // Mat drawing = Mat::zeros( frame.size(), CV_8UC3 );
-       	// Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       	// for( int k = 0; k< contours.size(); k++ )
-       	// {
-        //     Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-            
-        //    	drawContours( drawing, contours, k, color, 2, 8, hierarchy, 0, Point() );
-        //    	printf("dddd \n");
-       	// }
         
+        findContours(morph_frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
+        for (int j=0; j<contours.size();++j)
+        { 
+            double area;
+            area = contourArea(contours[j]);
+            if (area > 80 && area < 150)
+            {
+                modifed_contours.push_back(contours[j]);
+            }
+        }
+
+        cout << modifed_contours.size() << endl;
+        Mat drawing = Mat::zeros( morph_frame.size(), CV_8UC3 );
+
+       	for( int k = 0; k< modifed_contours.size(); k++ )
+       	{
+            Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+            
+           	drawContours( drawing, modifed_contours, k, color, 2, 8, hierarchy, 0, Point() );
+       	}
+        
+        imshow("Black Beads", drawing);
+        if(waitKey(30) >= 0) break;
        	// printf("here");
        	// fitEllipse(contours);
        	// printf("finish fitting");
