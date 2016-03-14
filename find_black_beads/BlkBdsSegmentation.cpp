a5adb041f1c8c0dbf2e2061c601345ffc9c1f335
lusu8892 <sxl924@case.edu>
2016-03-14 11:24:26 -0400

    updated

--- find_black_beads/BlkBdsSegmentation.cpp
+++ find_black_beads/BlkBdsSegmentation.cpp
@@ -22,19 +22,27 @@ int main(int argc, char** argv )
     string file_name;
     char buffer [50];
 
+    Mat frame;
+    Mat cvt_frame;
+    Mat threshold_frame;
+    Mat morph_frame;
+
+	vector<vector<Point> > contours;
+    vector<vector<Point> > modifed_contours;
+
+    vector<Vec4i> hierarchy;
 
     for(int i=0 ;; i++)
     {
-        Mat frame;
-        Mat cvt_frame;
-        Mat threshold_frame;
-        Mat morph_frame;
+        frame.release();
+        cvt_frame.release();
+        threshold_frame.release();
+        morph_frame.release();
 
-        vector<vector<Point> > contours;
-        vector<vector<Point> > modifed_contours;
+        contours.clear();
+        modifed_contours.clear();
 
-        vector<Vec4i> hierarchy;
-        vector<double> area_vec;
+        hierarchy.clear();
 
         cap >> frame; // get a new frame from camera
         cvtColor(frame, cvt_frame, CV_BGR2GRAY); // convert color image to gray image
@@ -43,17 +51,19 @@ int main(int argc, char** argv )
         threshold(cvt_frame, threshold_frame, 150, 255, THRESH_BINARY_INV+THRESH_OTSU); // segmentation
         
         morphologyEx(threshold_frame, morph_frame, MORPH_CLOSE, getStructuringElement(MORPH_ELLIPSE,Size(3,3)));
-        imshow("Black Beads", morph_frame);
-        if(waitKey(30) >= 0) break;
+        // imshow("Black Beads", morph_frame);
+        // if(waitKey(30) >= 0) break;
         
         findContours(morph_frame, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
         for (int j=0; j<contours.size();++j)
         { 
             double area;
             area = contourArea(contours[j]);
-            if (area > 80 && area < 150)
+            if (area > 500 && area < 800)
             {
                 modifed_contours.push_back(contours[j]);
+                printf("Area: %f \n",area);
+
             }
         }
 
